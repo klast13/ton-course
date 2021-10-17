0d71e58cc22b3a2a3c5629f31463aab338e38e98
klast <klast13@mail.ru>
2021-10-17 23:56:43 +0300

    task3

---
+++ task3/queue.sol
@@ -0,0 +1,35 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Accumulator {
+
+	// State variable storing the sum of arguments that were passed to function 'add',
+    string [] public names;
+
+	constructor() public {
+		// check that contract's public key is set
+		require(tvm.pubkey() != 0, 101);
+		// Check that message has signature (msg.pubkey() is not zero) and message is signed with the owner's private key
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	// Modifier that allows to accept some external messages
+	modifier checkOwnerAndAccept {
+		// Check that message was signed with contracts key.
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	function get_to_queue(string _name) public checkOwnerAndAccept {
+        names.push(_name);
+        return;
+	}
+
+	function call_next() public checkOwnerAndAccept {
+        delete names[0];
+        return;
+	}
+
+}
