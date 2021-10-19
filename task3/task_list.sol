a67b3d1989241413c71d08240791b9ccd4194c0e
klast <klast13@mail.ru>
2021-10-19 23:26:23 +0300

    tasks3 with mapping

--- task3/task_list.sol
+++ task3/task_list.sol
@@ -1,21 +1,16 @@
-// я не смог разобраться как правильно работать с массивом структур в солидити, контракт не работает, написал схематично, чтобы показать идею 
-
-
-
-
 pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
 contract task_list {
 
-	// State variable storing the sum of arguments that were passed to function 'add',
     struct tasksStruct {
         string name;
-        uint32 timestamp;
         bool is_done;
+        bool is_deleted;
     }
-
-    tasksStruct[] tasks;
+    //<map>.min() не получилось со строкой сделать, было бы логичнее сделать ключом название
+    mapping(uint => tasksStruct) tasks;    
+    
 
 	constructor() public {
 		// check that contract's public key is set
@@ -25,52 +20,77 @@ contract task_list {
 		tvm.accept();
 	}
 
-	// Modifier that allows to accept some external messages
 	modifier checkOwnerAndAccept {
 		// Check that message was signed with contracts key.
 		require(msg.pubkey() == tvm.pubkey(), 102);
 		tvm.accept();
 		_;
 	}
-
     // добавление новой задачи
-    function add_task(string _name) public {
-        tasksStruct newTask = tasksStruct(_name, now, false);
-        tasks.push(newTask);
+    function add_task(string _name) public checkOwnerAndAccept {
+        tasksStruct newTask = tasksStruct(_name, false, false);
+        tasks.add(now, newTask);        
 	}
-
     // показать кол-во невыполненых задач
-    function show_opened_tasks() public view returns (uint) {
+    function show_opened_tasks() public checkOwnerAndAccept returns (uint) {
         uint count_of_opened_tasks = 0;
-        for (uint i=0; i<tasks.length; i++){
-            if (!tasks[i].is_done) {
+        optional(uint, tasksStruct) currentOpt = tasks.min();
+        
+        while (currentOpt.hasValue()) {
+            (uint key, tasksStruct val) = currentOpt.get();
+            if (!val.is_done && !val.is_deleted) {
                 count_of_opened_tasks += 1; 
             }
         }
-        return count_of_opened_tasks;
+        return count_of_opened_tasks; 
 	}
-    
     // показать список задач
-    function show_list_of_tasks() public view checkOwnerAndAccept returns(tasksStruct[]) {        
-        return tasks;
-
-    // показать описание по ключу
-	}
-    function show_discribtion_by_key(uint index) public view checkOwnerAndAccept returns(tasksStruct) {
-        return tasks[index-1];
-
-    // удалить задачу
-	}
-    function delete_task(uint index) public checkOwnerAndAccept {
-        for (uint i = index; i < tasks.length - 1; i++) {
-            tasks[i] = tasks[i+1];
+    function show_list_of_tasks() public checkOwnerAndAccept returns(tasksStruct[]) { 
+        tasksStruct[] resArr;       
+        optional(uint, tasksStruct) currentOpt = tasks.min();
+        
+        while (currentOpt.hasValue()) {
+            (uint key, tasksStruct val) = currentOpt.get();
+            resArr.push(val);
+            currentOpt = tasks.next(key);
         }
-        tasks.pop();
-
+        return resArr; 
+    }
+    // показать описание по ключу
+	    function show_discribtion_by_key(string _name) public checkOwnerAndAccept returns(tasksStruct) {
+        optional(uint, tasksStruct) currentOpt = tasks.min();
+        
+        while (currentOpt.hasValue()) {
+            (uint key, tasksStruct val) = currentOpt.get();
+            if (val.name == _name) {
+                return val;
+            }
+            currentOpt = tasks.next(key);
+        }
+    }
+    // удалить задачу
+    function delete_task(string _name) public view checkOwnerAndAccept {
+        optional(uint, tasksStruct) currentOpt = tasks.min();
+        
+        while (currentOpt.hasValue()) {
+            (uint key, tasksStruct val) = currentOpt.get();
+            if (val.name == _name) {
+                val.is_deleted = true;
+            }
+            currentOpt = tasks.next(key);
+        }
+    }   
     // пометить как выполненную
-	}
-    function mark_as_done(uint index) public checkOwnerAndAccept {
-        tasks[index].is_done = true;
-	}
+    function mark_as_done(string _name) public view checkOwnerAndAccept {
+        optional(uint, tasksStruct) currentOpt = tasks.min();
+        
+        while (currentOpt.hasValue()) {
+            (uint key, tasksStruct val) = currentOpt.get();
+            if (val.name == _name) {
+                val.is_done = true;
+            }
+            currentOpt = tasks.next(key);
+	    }
+    }
 
 }
